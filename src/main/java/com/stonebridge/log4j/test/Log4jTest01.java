22ef95b5278b61a621f420c0953beade09d3c2a0
stonebridge <stonebridge@njfu.edu.cn>
2022-03-21 21:13:53 +0800

    日志输出详细信息开关

--- src/main/java/com/stonebridge/log4j/test/Log4jTest01.java
+++ src/main/java/com/stonebridge/log4j/test/Log4jTest01.java
@@ -2,6 +2,7 @@ package com.stonebridge.log4j.test;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.helpers.LogLog;
 
 public class Log4jTest01 {
     public void test01() {
@@ -137,5 +138,32 @@ public class Log4jTest01 {
 
     }
 
+    public void test03() {
 
+        /*
+
+            通过Logger中的开关
+            打开日志输出的详细信息
+            查看LogManager类中的方法
+            getLoggerRepository()
+            找到代码LogLog.debug(msg, ex);表示LogLog会使用debug级别的输出为我们展现日志输出详细信息
+            Logger是记录系统的日志，那么LogLog是用来记录Logger的日志
+
+            进入到LogLog.debug(msg, ex);方法中
+            通过代码：if (debugEnabled && !quietMode) {
+            观察到if判断中的这两个开关都必须开启才行
+            !quietMode是已经启动的状态，不需要我们去管
+            debugEnabled默认是关闭的
+            所以我们只需要设置debugEnabled为true就可以了
+         */
+
+        LogLog.setInternalDebugging(true);
+        Logger logger = Logger.getLogger(Log4jTest01.class);
+        logger.fatal("fatal信息");
+        logger.error("error信息");
+        logger.warn("warn信息");
+        logger.info("info信息");
+        logger.debug("debug信息");
+        logger.trace("trace信息");
+    }
 }
